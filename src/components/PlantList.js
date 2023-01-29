349eba19373ad256811a1ad7ad892c2a88c39149
lizdmukami <lizdmukami@gmail.com>
2023-01-29 06:06:43 +0300

    display all plants when page is rendered

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(res=> res.json())
+    .then((data) => setPlants(() => data))
+  }, [])
+
+  const plantsLoop = plants.map((plant) => {
+    return (
+      <PlantCard key={plant.id} id={plant.id} name={plant.name} image={plant.image} price={plant.price} />
+    )
+  }) 
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plantsLoop}
+    </ul>
   );
 }
 
